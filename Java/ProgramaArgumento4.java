70ae4e4416f27de466d11f57cbf46270fe5572e6
Franrestrepo <francisco.restrepof@campusucc.edu.co>
2020-10-17 16:03:20 -0500

    Ejercicios Java

---
+++ Java/ProgramaArgumento4.java
@@ -0,0 +1,68 @@
+public class ProgramaArgumento4 {
+  public static void main(String[] args) {
+      System.out.println("*-------------------------------------*");
+	  System.out.println("*                                     *");
+	  System.out.println("*                                     *");
+	  System.out.println("*                                     *");
+	  System.out.println("*Autor: Francisco Javier Restrepo F   *");
+	  System.out.println("*Fecha: 17 de Octubre de 2020         *");
+	  System.out.println("*                                     *");
+	  System.out.println("*                                     *");
+	  System.out.println("*-------------------------------------*");
+	  System.out.println("*#####################################*");
+	  System.out.println("*Programa Argument division de enteros*");
+	  System.out.println("*#####################################*");
+	  System.out.println("*                                     *");
+	  System.out.println("*-------------------------------------*");
+	  System.out.println("_____________________$$$_______________");
+      System.out.println("____________________$___$______________");
+      System.out.println("_____________________$$$_______________");
+      System.out.println("_____________________$_$_______________");
+      System.out.println("_____________________$_$_______________");
+      System.out.println("___________________$$$_$$$_____________");
+      System.out.println("_________________$$__$$$__$$$__________");
+      System.out.println("_______________$$__$$$$$$$___$_________");
+      System.out.println("______________$_______________$________");
+      System.out.println("_____________$_________________$_______");
+      System.out.println("_____________$_________________$_______");
+      System.out.println("_____________$_____$$$$$$$$$$$$$$$_____");
+      System.out.println("_____________$____$_______________$____");
+      System.out.println("_____________$____$___$$$$$$$$$$$$$____");
+      System.out.println("_____________$___$___$___________$$$___");
+      System.out.println("_____________$___$___$_$$$___$$$__$$___");
+      System.out.println("_____________$___$___$_$$$___$$$__$$___");
+      System.out.println("_____________$___$___$___________$$$___");
+      System.out.println("_____________$____$___$$$$$$$$$$$$$____");
+      System.out.println("_____________$_____$$$$$$$$$$$$$$______");
+      System.out.println("_____________$_________________$_______");
+      System.out.println("_____________$____$$$$$$$$$$$$$$_______");
+      System.out.println("_____________$___$__$__$__$__$_________");
+      System.out.println("_____________$__$$$$$$$$$$$$$$_________");
+      System.out.println("_____________$__$___$__$__$__$_________");
+      System.out.println("_____________$___$$$$$$$$$$$$$$$_______");
+      System.out.println("____________$$$_________________$$$____");
+      System.out.println("__________$$___$$$_________$$$$$___$$__");
+      System.out.println("________$$________$$$$$$$$$__________$$$");
+      System.out.println("_______$__$$_____________________$$$$___$$");
+      System.out.println("____$$$$$___$$$$$$$$______$$$$$$$_______$_$");
+      System.out.println("__$______$$_________$$$$$$______________$_$$");
+      System.out.println("_$____$____$____________________________$_$_$");
+      System.out.println("_$_____$___$______________$$$$$$$$$$$___$_$_$$");
+      System.out.println("_$$$____$___$__$$$$$$$$$$$$__________$___$_$_$$");
+      System.out.println("$___$$$$____$__$_____________________$___$_$$_$");
+      System.out.println("$$$____$___$$__$_____________________$$__$_$__$");
+      System.out.println("$___$__$__$$___$______________________$__$$$__$");
+      System.out.println("$_____$$_$$____$_______________$$$____$__$_$__$");
+	  System.out.println();
+	  
+	  System.out.println("Argumento 1 = "+args[0]);
+	  System.out.println("Argumento 2 = "+args[0]);
+	  
+	  int num1 = Integer.parseInt(args[0]);
+	  int num2 = Integer.parseInt(args[1]);;
+	  int resulDivision = num1 / num2;
+	  
+	  System.out.println("Operacion matematica de num1 = "+num1+", num2 = "+num2);
+	  System.out.println("Resultado de la Resta es = "+resulDivision);
+  }
+  }
---
+++ Java/ProgramaHolaMundo.java
@@ -0,0 +1,33 @@
+public class ProgramaHolaMundo {
+  public static void main(String[] args) {
+	  System.out.println("*-------------------------------------*");
+	  System.out.println("*                                     *");
+	  System.out.println("*                                     *");
+	  System.out.println("*                                     *");
+	  System.out.println("*Autor: Francisco Javier Restrepo F   *");
+	  System.out.println("*Fecha: 17 de Octubre de 2020         *");
+	  System.out.println("*                                     *");
+	  System.out.println("*                                     *");
+	  System.out.println("*-------------------------------------*");
+	  System.out.println("░░░░░░░░░░░░▄▄▄█▀▀▀▀▀▀▀▀█▄▄▄░░░░░░░░░░░░");
+      System.out.println("░░░░░░░░▄▄█▀▀░░░░░░░░░░░░░░▀▀█▄▄░░░░░░░░");
+      System.out.println("░░░░░░▄█▀░░░░▄▄▄▄▄▄▄░░░░░░░░░░░▀█▄░░░░░░");
+	  System.out.println("░░░░▄█▀░░░▄██▄▄▄▄▄▄▄██▄░░░░▄█▀▀▀▀██▄░░░░");
+      System.out.println("░░░█▀░░░░█▀▀▀░░▄██░░▄▄█░░░██▀▀▀███▄██░░░");
+      System.out.println("░░█░░░░░░▀█▀▀▀▀▀▀▀▀▀██▀░░░▀█▀▀▀▀███▄▄█░░");
+      System.out.println("░█░░░░░░░░░▀▀█▄▄██▀▀░░░░░░░░▀▄▄▄░░░▄▄▀█░");
+      System.out.println("█▀░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▀▀▀▀▀░░▀█");
+      System.out.println("█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▄░░░░█");
+      System.out.println("█░░░░░░░░░░░░░░░░░░░░░░░░▄▄▄▄▄██░░▀█░░░█");
+      System.out.println("█░░░░░░░░░░░░░░█░░░▄▄▄█▀▀▀░░░▄█▀░░░░░░░█");
+      System.out.println("█░░░░░░░░░░░░░░░░░░▀░░░░░░░░█▀░░░░░░░░░█");
+      System.out.println("█▄░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▄█");
+      System.out.println("░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█░");
+      System.out.println("░░█░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█░░");
+      System.out.println("░░░█▄░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▄█░░░");
+      System.out.println("░░░░▀█▄░░░░░░░░░░░░░░░░░░░░░░░░░░▄█▀░░░░");
+      System.out.println("░░░░░░▀█▄░░░░░░░░░░░░░░░░░░░░░░▄█▀░░░░░░");
+	  System.out.println();
+    System.out.println("Este es mi primer programa en JAVA,este es el segundo ejemplo");
+  }
+}
